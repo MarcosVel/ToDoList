4ebfde09dd9628c72353e5c234f082691c900d85
MarcosVel <marcos.veloso@sempreceub.com>
2021-10-04 23:46:11 -0300

    mapping list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import * as C from "./App.styles";
+import { Item } from './types/Item';
 
 const App = () => {
+  const [ list, setList ] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão', done: false },
+    { id: 2, name: 'Comprar bolo', done: false },
+  ]);
+
   return (
     <C.Container>
-      
+      <C.Area>
+        <C.Header>Lista de Tarefas</C.Header>
+        {/* Área de add nova tarefa */}
+        {list.map((item, index) => (
+          <div>...</div>
+        ))}
+      </C.Area>
     </C.Container>
   )
 }
